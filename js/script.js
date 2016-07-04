c62d61740c5f76786de7253630283c2029a492f5
johney10 <jburke@jburke1029.local>
2016-07-04 16:20:36 -0600

    initial setup

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//$(document).ready function(){
+
+
+	function learnMore(){
+
+
+		$("#show-this-on-click").slideDown();
+		$(".readmore").hide();
+		$(".readless").show();
+	 
+	}
+
+	$(".readmore").click(learnMore);
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}
